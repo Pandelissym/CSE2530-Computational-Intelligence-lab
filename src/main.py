61d3b49d642d5c100aa1766fb63eaca9ebb7e0e8
Amar Mesic <A.Mesic@student.tudelft.nl>
2021-03-05 22:12:44 +0100

    Predict with Final Architecture

--- src/main.py
+++ src/main.py
@@ -25,8 +25,9 @@ def main():
     training_data, test_data = split_train_test(data, 0.15)
     training_data, validation_data = split_train_test(training_data, 0.15)
 
-    network = Network([10, 20, 7])
-    network.train(training_data, 35, 20, 0.07, validation_data)
+    network = Network([10, 30, 7])
+    # Set to data because we are testing it on the unkown dataset
+    network.train(data, 35, 20, 0.07, validation_data)
 
     # # Plots
     accuracy = network.evaluate(test_data)
@@ -39,30 +40,29 @@ def main():
     plt.legend()
     plt.show()
 
-    # Plot performance of different middle layers
-    neuron_layouts = {
-        'amounts': [5, 8, 10, 20, 30, 45],
-        'scores': []
-    }
-    print('\nk-fold validating...')
-    for amount in neuron_layouts['amounts']:
-        targets_true = split_train_test(true_targets(), 1 - (1 - 0.15) ** 2)[0]
-        k_fold_acc = cross_validate(Network([10, amount, 7]), np.array(training_data, dtype=object), targets_true,
-                                    validation_data=np.array(validation_data) if amount == 30 else None, epochs=24, k=10)
-        print('\nk_fold_acc: ', k_fold_acc)
-        neuron_layouts['scores'].append(round(k_fold_acc, 2))
-    plot_results(neuron_layouts)
+    # # Plot performance of different middle layers
+    # neuron_layouts = {
+    #     'amounts': [5, 8, 10, 20, 30, 45],
+    #     'scores': []
+    # }
+    # print('\nk-fold validating...')
+    # for amount in neuron_layouts['amounts']:
+    #     targets_true = split_train_test(true_targets(), 1 - (1 - 0.15) ** 2)[0]
+    #     k_fold_acc = cross_validate(Network([10, amount, 7]), np.array(training_data, dtype=object), targets_true,
+    #                                 validation_data=np.array(validation_data) if amount == 30 else None, epochs=24, k=10)
+    #     print('\nk_fold_acc: ', k_fold_acc)
+    #     neuron_layouts['scores'].append(round(k_fold_acc, 2))
+    # plot_results(neuron_layouts)
 
-    # Write predictions of unknown data to Text File
-    f = open("../doc/Group_66_classes.txt", "w")
-    for x in unknown[:-1]:
-        print(x)
-        prediction = np.argmax(network.feedforward(x)) + 1
-        f.write(f"{prediction},")
-
-    prediction = np.argmax(network.feedforward(unknown[-1])) + 1
-    f.write(f"{prediction}")
-    f.close()
+    # # Write predictions of unknown data to Text File
+    # f = open("../doc/Group_66_classes.txt", "w")
+    # for x in unknown[:-1]:
+    #     prediction = np.argmax(network.feedforward(x)) + 1
+    #     f.write(f"{prediction},")
+    #
+    # prediction = np.argmax(network.feedforward(unknown[-1])) + 1
+    # f.write(f"{prediction}")
+    # f.close()
 
 
 
